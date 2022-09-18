ab254c446b8b4c8fa80ac871510b130b5d48cf38
Nazar <nazarii.sennyk@globallogic.com>
2022-09-18 11:29:38 +0300

    HW_1

---
+++ HW_1.py
@@ -0,0 +1,40 @@
+
+
+# Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+
+first = 10
+second = 30
+third = first + second
+print(third)
+
+third = first - second
+print(third)
+
+third = first * second
+print(third)
+
+third = first / second
+print(third)
+
+third = first ** second
+print(third)
+
+third = first // second
+print(third)
+
+third = first % second
+print(first)
+
+# Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+
+third = first < second
+print(third)
+
+third = first > second
+print(third)
+
+third = first == second
+print(third)
+
+third = first != second
+print(third)
