b604b20397780eac9752bc2109eba9214afca994
Nazar <nazarii.sennyk@globallogic.com>
2022-09-26 09:30:40 +0300

    HW_l

--- HW_3_2.py
+++ HW_3_2.py
@@ -1,13 +1,12 @@
 # Зформуйте строку, яка містить певну інформацію про символ в відомому слові. Наприклад "The [номер символу] symbol in [тут слово] is '[символ з відповідним порядковим номером]'".
 # Слово та номер отримайте за допомогою input() або скористайтеся константою. Наприклад (слово - "Python" а номер символу 3) - "The 3 symbol in "Python" is 't' ".
+
 user_word = input('Enter your word:')
 user_symbol = input('Enter your symbol:')
-if user_symbol == str(user_symbol):
-    print('Enter letter')
-else:
-    user_symbol = int(user_symbol)
-    resualt = user_word[user_symbol]
-    print(f' "The {user_symbol} symbol in \'{user_word}\' is \'{resualt}\' "')
+user_symbol = int(user_symbol)
+resualt = user_word[user_symbol]
+print(f' "The {user_symbol} symbol in \'{user_word}\' is \'{resualt}\' "')
+
 
 #Написати цикл, який буде вимагати від користувача ввести слово, в якому є буква "о" (враховуються як великі так і маленькі).
 # Цикл не повинен завершитися, якщо користувач ввів слово без букви о.
