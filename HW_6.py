0fafc2b4940c8379cd3dc41467ea76ffda7ff303
Nazar <nazarii.sennyk@globallogic.com>
2022-10-07 13:51:08 +0300

    HW_6 final

--- HW_6.py
+++ HW_6.py
@@ -1,7 +1,6 @@
-#1. вивести список всіх астронавтів, що перебувають в даний момент на орбіті (дані не фейкові, оновлюються в режимі реального часу)
 import requests
 from pprint import pprint
-import time
+
 
 url = 'http://api.open-notify.org/astros.json'
 response = requests.get(url)
@@ -9,18 +8,5 @@ response_json = response.json()
 people = response_json['people']
 name = 'name'
 people = [b_dict[name] for b_dict in people]
-pprint(people, width=200)
-
-
-
-
-#вивести список всіх астронавтів, що перебувають в даний момент на орбіті (дані не фейкові, оновлюються в режимі реального часу)
-
-import requests
-
-url = 'http://api.open-notify.org/astros.json'
-response = requests.get(url)
-astros = response.json()
-
-for astro in astros['people']:
-    print(astro['name'])
+for person in people:
+    pprint(person, width=200)
