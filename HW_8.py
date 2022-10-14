690c453078ccb7ceab2918f37234fe259b5c6c20
Nazar <nazarii.sennyk@globallogic.com>
2022-10-14 09:00:24 +0300

    HW_8vr

--- HW_8.py
+++ HW_8.py
@@ -1,21 +1,16 @@
 # # Напишіть декоратор, який перетворює результат роботи функції на стрінг
 # # Напишіть докстрінг для цього декоратора
-
-
 def outer_decorator(func):
     """This decorator transforms data from other function in to a str """
-    def inner_decorator(*args):
-            result = str(func(*args))
-            return result
+    def inner_decorator(*args,**kwargs):
+        result = str(func(*args))
+        return result
     return inner_decorator
 
 @outer_decorator
 def input_in_list():
     user_input = list(input('Please enter something:'))
     return user_input
-
 result = input_in_list()
-
 print(type(result))
-
 
