924576551e164ce330cf9fa076a75f9719972b68
Nazar <nazarii.sennyk@globallogic.com>
2022-10-17 22:25:38 +0300

    HW_9v3

--- HW_9.py
+++ HW_9.py
@@ -1,44 +1,51 @@
+def decorator(arg):
+    def decor_print_result(func):
+        """ Returns printed result of function in it
+        :param func: function
+        :return: printed result of function in args
+        """
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(func)
+            return result
+        return wrapper
+    return decorator
 
-def decor_print_result(func):
+
+@decorator('Function that checks if given argument is even or not')
+def odd_check(integer_float):
     """
-    :param func: function
-    :return: printed result of function in args
+    Function that checks if given argument is even or not
+    :return: True if argument % 2 == 0 else return False
     """
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        return result
-    return print((str(wrapper(4))), """
-    :param func: function
-    :return: printed result of function in args
-    """)
-@decor_print_result
-def odd_check(integer):
-    """
-    :param integer: int
-    :return: True if integer % 2 == 0 else return False
-    """
-    if type(integer) is int and float and integer % 2 == 0:
+    if integer_float % 2 == 0:
         return True
     else:
         return False
-assert odd_check('str') is False, 'Error'
-assert odd_check(1.54) is False, 'Error'
-assert odd_check(100) is True, 'Error'
-assert odd_check(-10) is True, 'Error'
-assert odd_check(' ') is False, 'Error'
-@decor_print_result
+
+
+assert odd_check(0)
+assert odd_check(1.54)
+assert odd_check(100)
+assert odd_check(-10)
+assert odd_check(0.213)
+
+
+@decorator('Function checks if given arg starts with capitalize letter')
 def capitalize_check(arg):
     """
-    Function checks if given arg is .capitalize()
+    Function checks if given arg starts with capitalize letter
     :param arg string
     :return: True if string == .capitalize() else return False
     """
-    res = True if arg == str(arg).capitalize() else False
-    return res
-assert capitalize_check('dadas') is False, 'Error'
-assert capitalize_check(54353) is False, 'Error'
-assert capitalize_check('234') is True, 'Error'
-assert capitalize_check('  ') is True, 'Error'
-assert capitalize_check('Rd322ffA') is False,'Error'
+    if arg == arg.capitalize():
+        return True
+    else:
+        return False
 
 
+assert capitalize_check('dadas')
+assert capitalize_check('234')
+assert capitalize_check('  ')
+assert capitalize_check('Rd322ffA')
+assert capitalize_check('23.45')
