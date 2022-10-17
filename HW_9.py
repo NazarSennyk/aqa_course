fea4642c8684e45a9215e65f0e59052139f9e30e
Nazar <nazarii.sennyk@globallogic.com>
2022-10-17 20:43:06 +0300

    HW_9vr.2

--- HW_9.py
+++ HW_9.py
@@ -1,9 +1,4 @@
-# напишіть функцію, яка перевірить, чи передане їй число є парним чи непарним (повертає True False)
-# напишіть функцію. яка приймає стрічку, і визначає, чи дана стрічка відповідає результатам роботи методу capitalize() (перший символ є верхнім регістром, а решта – нижнім.)
-# (повертає True False)
-# написати до кожної функції мінімум 5 assert
-# написати декоратор, який добавляє принт з результатом роботи отриманої функції + текстовий параметр, отриманий ним (декоратор з параметром - це там, де три функції)
-# при цьому очікувані результати роботи функції не змінюються (декоратор просто добавляє принт)
+
 def decor_print_result(func):
     """
     :param func: function
@@ -12,7 +7,7 @@ def decor_print_result(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         return result
-    return print(str(wrapper()), """
+    return print((str(wrapper(4))), """
     :param func: function
     :return: printed result of function in args
     """)
@@ -22,15 +17,15 @@ def odd_check(integer):
     :param integer: int
     :return: True if integer % 2 == 0 else return False
     """
-    if integer % 2 == 1:
-        return False
-    if integer % 2 == 0:
+    if type(integer) is int and float and integer % 2 == 0:
         return True
-assert type(odd_check(' ')) is True, 'Error'
-assert odd_check(1.54) is True, 'Error'
-assert odd_check('sdsa') is True, 'Error'
-assert odd_check() is True, 'Error'
-assert odd_check(' 353') is True,'Error'
+    else:
+        return False
+assert odd_check('str') is False, 'Error'
+assert odd_check(1.54) is False, 'Error'
+assert odd_check(100) is True, 'Error'
+assert odd_check(-10) is True, 'Error'
+assert odd_check(' ') is False, 'Error'
 @decor_print_result
 def capitalize_check(arg):
     """
@@ -38,14 +33,12 @@ def capitalize_check(arg):
     :param arg string
     :return: True if string == .capitalize() else return False
     """
-    if arg == arg.capitalize():
-        return True
-    else:
-        return False
-assert capitalize_check('12345') is True, 'Error'
-assert capitalize_check(54353) is True, 'Error'
-assert capitalize_check('sdada') is True, 'Error'
+    res = True if arg == str(arg).capitalize() else False
+    return res
+assert capitalize_check('dadas') is False, 'Error'
+assert capitalize_check(54353) is False, 'Error'
+assert capitalize_check('234') is True, 'Error'
 assert capitalize_check('  ') is True, 'Error'
-assert capitalize_check('   fdfddAsad') is True,'Error'
+assert capitalize_check('Rd322ffA') is False,'Error'
 
 
