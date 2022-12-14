2e93575735f405aa597bf32a5cffdab3db06cb6e
Nazar <nazar.sennyk@gmail.com>
2022-12-14 14:27:14 +0200

    hw_db_v3

--- db_repositories/base_repository.py
+++ db_repositories/base_repository.py
@@ -14,14 +14,6 @@ class BaseRepo:
         self._cursor.execute(f"select * from '{self.table_name}';")
         return self._cursor.fetchall()
 
-    def create_table_orders(self):
-        self._cursor.execute(f"create table orders ( id int primary key, product_id int, quantity int);")
-        return self._cursor.fetchone()
-
-    def crate_table_products(self):
-        self._cursor.execute(f"create table products (id int primary key, name varchar (25), price int);")
-        return self._cursor.fetchone()
-
     def __del__(self):
         if self._connection:
             if self._cursor:
---
+++ db_repositories/table_repository.py
@@ -0,0 +1,15 @@
+from qa_automation_hw.db_repositories.base_repository import BaseRepo
+
+
+class TableRepo(BaseRepo):
+    def __init__(self):
+        super().__init__()
+        self.table_name = 'users'
+
+    def create_table_orders(self):
+        self._cursor.execute(f"create table orders ( id int primary key, product_id int, quantity int);")
+        return self._cursor.fetchone()
+
+    def crate_table_products(self):
+        self._cursor.execute(f"create table products (id int primary key, name varchar (25), price int);")
+        return self._cursor.fetchone()
