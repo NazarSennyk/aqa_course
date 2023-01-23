30ea6f0d736b25b16d426d2869ab14c8e82a6200
Nazar <nazar.sennyk@gmail.com>
2023-01-23 18:28:47 +0200

    OOP_hw3_v2

--- oop_hw3_v2/train.py
+++ oop_hw3_v2/train.py
@@ -1,20 +1,23 @@
-from random import randint
-from faker import Faker
-
-fake = Faker()
 
 
 class Train:
+
     def __init__(self):
         self.__locomotive = 1
-        self.list_of_wagons = [1]
-        self.list_of_passengers = [fake.unique.first_name() for i in range(10)]
+        self.list_of_wagons = []
 
-    def __str__(self):
-        return f' {self.__class__.__name__}: {{\n\twagon: {self.list_of_wagons}\n\t passengers:{self.list_of_passengers}\t}}'
+    def __len__(self):
+        return len(self.list_of_wagons)
 
-    def add_wagon(self):
-        self.list_of_wagons += [randint(1, 20)]
+    def add_wagon(self, wagon: int):
+        return self.list_of_wagons.append(wagon)
 
-c = Train()
-print(c)
+
+
+trn = Train()
+
+trn.add_wagon(1)
+trn.add_wagon(2)
+trn.add_wagon(3)
+
+print(len(trn.list_of_wagons))
--- oop_hw3_v2/wagon.py
+++ oop_hw3_v2/wagon.py
@@ -1,17 +1,31 @@
+from faker import Faker
+
 from aqa_course.oop_hw3_v2.train import Train
 
+fake = Faker()
 
-class Wagon(Train):
+
+class Wagon:
+
     def __init__(self):
-        super().__init__()
+        self.wagon_number = [Train().__getattribute__('list_of_wagons')]
+        self.list_of_passengers = [fake.unique.first_name() for i in range(10)]
+
+    def __str__(self):
+        return f' {self.__class__.__name__}: {{\n\twagon: {self.wagon_number}\n\t passengers:{self.list_of_passengers}' \
+               f'\n\t}}'
 
     def add_passenger(self, passenger: str):
         return self.list_of_passengers.append(passenger)
 
-    def __len__(self, arg):
-        if arg == self:
-            return len(self.list_of_wagons)
-        elif arg == Wagon:
-            return len(self.list_of_passengers)
+    def __len__(self):
+        return len(self.list_of_passengers)
+
 
 c = Wagon()
+
+
+print(len(c.wagon_number))
+
+print(c)
+
