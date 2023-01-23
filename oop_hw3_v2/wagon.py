93dcfd458dd6f6d9ca69962c7b581585e1f4f5c6
Nazar <nazar.sennyk@gmail.com>
2023-01-23 12:05:04 +0200

    new

---
+++ oop_hw3_v2/wagon.py
@@ -0,0 +1,17 @@
+from aqa_course.oop_hw3_v2.train import Train
+
+
+class Wagon(Train):
+    def __init__(self):
+        super().__init__()
+
+    def add_passenger(self, passenger: str):
+        return self.list_of_passengers.append(passenger)
+
+    def __len__(self, arg):
+        if arg == self:
+            return len(self.list_of_wagons)
+        elif arg == Wagon:
+            return len(self.list_of_passengers)
+
+c = Wagon()
