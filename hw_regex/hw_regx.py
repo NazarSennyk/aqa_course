d000fa4a5ef6850dfb76752c00117d50381afa72
Nazar <nazar.sennyk@gmail.com>
2023-02-01 18:05:08 +0200

    HW_regex

---
+++ hw_regex/hw_regx.py
@@ -0,0 +1,39 @@
+import re
+from pprint import pprint
+# Task_1
+json_dict = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
+
+def convert_name1(name: str):
+    result1 = re.compile('[\w]+=\w+')
+    result2 = result1.findall(name)
+    final_dict = {}
+    for item in result2:
+        temp = item.split('=')
+        final_dict[temp[0]] = temp[1]
+    return final_dict
+
+
+print(convert_name1(json_dict))
+
+# Task_2
+words = '["FirstItem", "FriendsList", "MyTuple"]'
+
+
+def convert_name2(name: str):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1\2', name).replace('[', '')
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower().replace('"','').replace(']','').split(', ')
+
+
+print(convert_name2(words))
+
+# Tesk_3
+
+def regex_for_txt():
+    with open('text.txt') as file:
+        var_file = file.read()
+        regex = re.compile('\.|\s|^....\s')
+        regex_result = regex.sub(' ', var_file, 1)
+        return regex_result
+
+pprint(regex_for_txt())
+
